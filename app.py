1291cdaafe3710f21fd4b2b347cac0183d1b233c
Jason McCollum <jason.mccollum@woolpert.com>
2021-08-24 13:31:06 -0700

    Connect to database

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
+import psycopg2
 
 app = Flask(__name__)
+
+conn = psycopg2.connect("postgres://renderuser:95YvqCrPPcKt5Rro7oyPJOwvKt8wQlOl@oregon-postgres.render.com/renderdatabase")
 
 @app.get('/')
 def app_get():
